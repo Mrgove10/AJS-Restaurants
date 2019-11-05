b8091117da5ec3fa73918fe2d2bb9f8c7801a7b5
Adrien RICHARD <richardadrien0@gmail.com>
2019-11-05 11:23:55 +0100

    it works ? wtf

--- app/app.js
+++ app/app.js
@@ -35,9 +35,9 @@ angular.module('myApp', [])
           "first": "Lupe",
           "last": "Dudley"
         },
-        "dateLastVisite": "Mon Jun 09 1980 12:22:38 GMT+0000 (UTC)",
-        "lastcomment": "Laborum minim qui labore labore sit nisi incididunt et quis incididunt anim amet sunt ipsum. Elit incididunt in dolore tempor esse veniam. Minim deserunt laborum esse ullamco et quis cillum ullamco laboris dolore quis incididunt consequat minim. Pariatur sit qui irure nostrud culpa culpa ut. Veniam deserunt cillum duis exercitation do sunt labore sunt velit nisi ut.",
-        "noteLastVisite": 4
+        "dateLastVisite": "",
+        "lastcomment": "",
+        "noteLastVisite": ""
       },
       {
         "guid": "77715761-91c6-496b-8801-871488715b69",
@@ -54,9 +54,9 @@ angular.module('myApp', [])
           "first": "Bean",
           "last": "Flynn"
         },
-        "dateLastVisite": "Tue Nov 26 1991 22:06:38 GMT+0000 (UTC)",
-        "lastcomment": "Proident cillum consequat minim excepteur exercitation ex quis laborum dolore veniam deserunt. Non voluptate excepteur tempor consectetur anim cupidatat aliquip sit aute mollit esse labore amet. Cillum irure velit consequat cillum excepteur esse exercitation aute ipsum. Veniam deserunt ad aliqua pariatur elit Lorem proident est fugiat reprehenderit. Do adipisicing deserunt aliqua reprehenderit cillum ex.",
-        "noteLastVisite": 1
+        "dateLastVisite": "",
+        "lastcomment": "",
+        "noteLastVisite": ""
       },
       {
         "guid": "938e02aa-4e5a-499c-9052-597b1ee1a21c",
@@ -73,9 +73,9 @@ angular.module('myApp', [])
           "first": "Alta",
           "last": "Ortega"
         },
-        "dateLastVisite": "Fri Dec 05 2014 20:14:06 GMT+0000 (UTC)",
-        "lastcomment": "Lorem minim dolore velit commodo officia exercitation do culpa cillum commodo aliquip consectetur. Lorem id exercitation velit duis in laborum ea sint ullamco elit velit. Laborum aute et nulla officia est quis. Officia laboris non incididunt ullamco aute elit esse commodo reprehenderit esse. Excepteur excepteur in sint ex reprehenderit officia. Occaecat ex quis sunt sint nisi anim consequat. Magna sit sit reprehenderit cupidatat sint ut commodo pariatur fugiat reprehenderit aliquip occaecat ad.",
-        "noteLastVisite": 2
+        "dateLastVisite": "",
+        "lastcomment": "",
+        "noteLastVisite": ""
       },
       {
         "guid": "05fe7b1a-63da-4589-a1b8-76b893b7c8ab",
@@ -92,9 +92,9 @@ angular.module('myApp', [])
           "first": "May",
           "last": "Franklin"
         },
-        "dateLastVisite": "Sun Nov 12 1995 06:28:33 GMT+0000 (UTC)",
-        "lastcomment": "Enim aliqua proident sit labore. Non mollit ut dolore cupidatat. Adipisicing est et qui esse occaecat eiusmod voluptate Lorem duis tempor enim sit quis deserunt. Nostrud ad ea ipsum Lorem. Amet sunt irure id labore ex. Eiusmod in mollit Lorem commodo aliqua Lorem deserunt Lorem nostrud eu.",
-        "noteLastVisite": 5
+        "dateLastVisite": "",
+        "lastcomment": "",
+        "noteLastVisite": ""
       },
       {
         "guid": "1cd3e751-343f-4a24-9cb7-a7c8059dcd8f",
@@ -111,9 +111,9 @@ angular.module('myApp', [])
           "first": "Collier",
           "last": "Palmer"
         },
-        "dateLastVisite": "Sat Mar 31 2001 11:02:26 GMT+0000 (UTC)",
-        "lastcomment": "Minim anim in minim amet id ex adipisicing mollit tempor nulla nisi enim. Ea aute incididunt reprehenderit ut eu elit nostrud laborum sunt Lorem. Proident culpa veniam minim ex magna ex enim do nisi tempor laboris proident magna mollit.",
-        "noteLastVisite": 5
+        "dateLastVisite": "",
+        "lastcomment": "",
+        "noteLastVisite": ""
       },
       {
         "guid": "193d4231-9a43-471f-9217-282f2650545b",
@@ -130,9 +130,9 @@ angular.module('myApp', [])
           "first": "Ramona",
           "last": "Singleton"
         },
-        "dateLastVisite": "Sun Sep 18 1988 10:10:54 GMT+0000 (UTC)",
-        "lastcomment": "Ea do ullamco elit consequat velit aliquip nostrud occaecat. Sint aliquip qui occaecat tempor Lorem pariatur non id enim excepteur reprehenderit qui ipsum. Aliqua est proident consequat ex velit in commodo eu. Anim veniam dolore aute ea qui reprehenderit aliquip enim id incididunt. Exercitation amet mollit nulla deserunt dolore eu commodo dolore amet. Est ex nulla cillum non nisi pariatur incididunt aliqua tempor aliquip exercitation. Cillum ipsum ea et adipisicing labore esse tempor ea nulla aliquip ea incididunt nisi.",
-        "noteLastVisite": 3
+        "dateLastVisite": "",
+        "lastcomment": "",
+        "noteLastVisite":""
       }
     ]
   }]
