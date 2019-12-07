60eab49d6f759095e89a1c4d7b2abec06130aec6
Adrien RICHARD <richardadrien0@gmail.com>
2019-12-07 16:41:07 +0100

    remouved unsued list

--- app/views/restaurant/detaillist/restaurantdetaillist.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-angular.module('myApp').directive('restaurantdetaillist', [function () {
-    return {
-        templateUrl: 'views/restaurant/detaillist/restaurantdetaillist.html',
-        scope: {
-            restau: '='
-        }
-    }
-}]);
