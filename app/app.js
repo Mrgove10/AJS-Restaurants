0c6263685e98e444ebe5b745e4c4c3f5370ca27a
Adrien RICHARD <richardadrien0@gmail.com>
2019-12-07 17:24:24 +0100

    addedrestaurant

--- app/app.js
+++ app/app.js
@@ -6,12 +6,7 @@ angular.module('myApp', ['mainservice', 'ngRoute'])
         {
           "guid": "a5fd0333-ef4c-4920-8e86-7a7517152f51",
           "name": "Ray",
-          "address": {
-            "number": 766,
-            "road": "Sullivan Street",
-            "city": "Mathews",
-            "state": "New York"
-          },
+          "address": "dfhsdkfhsdjfbsjkskbfsd",
           "email": "a.a@gmail.com",
           "about": "Minim adipisicing laborum et eiusmod reprehenderit commodo exercitation officia adipisicing. Occaecat ut non nisi sunt fugiat quis eu veniam deserunt. Minim ad anim pariatur incididunt velit aute consectetur cupidatat cupidatat enim ea.",
           "ownerName": "Lupe Dudley",
@@ -22,12 +17,7 @@ angular.module('myApp', ['mainservice', 'ngRoute'])
         {
           "guid": "77715761-91c6-496b-8801-871488715b69",
           "name": "Hernandez",
-          "address": {
-            "number": 202,
-            "road": "Garden Place",
-            "city": "Utting",
-            "state": "Ohio"
-          },
+          "address": "dfhsdkfhsdjfbsjkskbfsd",
           "email": "a.a@gmail.com",
           "about": "Dolore voluptate veniam elit esse et dolor cupidatat ad ipsum ipsum consequat. Duis eu eu officia aliquip ea reprehenderit sit consectetur incididunt in. Nostrud cillum Lorem eiusmod nisi fugiat ipsum adipisicing. Non ea eu enim deserunt veniam dolor et fugiat duis voluptate.",
           "ownerName": "Bean Flynn",
@@ -38,12 +28,7 @@ angular.module('myApp', ['mainservice', 'ngRoute'])
         {
           "guid": "938e02aa-4e5a-499c-9052-597b1ee1a21c",
           "name": "Mcclain",
-          "address": {
-            "number": 241,
-            "road": "Forest Place",
-            "city": "Sultana",
-            "state": "Wisconson"
-          },
+          "address": "dfhsdkfhsdjfbsjkskbfsd",
           "email": "a.a@gmail.com",
           "about": "Occaecat laboris eiusmod exercitation ad. Ex Lorem minim nostrud aute proident esse ipsum minim in dolore nulla sint laborum. Elit ullamco id duis commodo et ea velit amet cillum. Occaecat ex ex id deserunt fugiat adipisicing do. Qui excepteur irure ad laboris. Commodo in deserunt ex eu voluptate cillum.",
           "ownerName": "Alta Ortega",
@@ -54,12 +39,7 @@ angular.module('myApp', ['mainservice', 'ngRoute'])
         {
           "guid": "05fe7b1a-63da-4589-a1b8-76b893b7c8ab",
           "name": "Shelton",
-          "address": {
-            "number": 419,
-            "road": "Kings Place",
-            "city": "Herlong",
-            "state": "California"
-          },
+          "address": "dfhsdkfhsdjfbsjkskbfsd",
           "email": "a.a@gmail.com",
           "about": "Mollit nulla exercitation non aliqua eu reprehenderit occaecat. Tempor non veniam reprehenderit velit adipisicing elit officia est ea cupidatat laboris exercitation enim reprehenderit. Lorem est velit sunt laboris dolore aute ea in. Voluptate quis nulla laborum adipisicing ea. Irure non occaecat culpa aute aliquip fugiat in adipisicing officia consequat veniam ipsum esse mollit. Est occaecat amet veniam magna anim nisi consequat sint dolor in sunt dolor id.",
           "ownerName": "May Franklin",
@@ -70,12 +50,7 @@ angular.module('myApp', ['mainservice', 'ngRoute'])
         {
           "guid": "1cd3e751-343f-4a24-9cb7-a7c8059dcd8f",
           "name": "Nguyen",
-          "address": {
-            "number": 876,
-            "road": "Cherry Street",
-            "city": "Cliffside",
-            "state": "Washinton"
-          },
+          "address": "dfhsdkfhsdjfbsjkskbfsd",
           "email": "a.a@gmail.com",
           "about": "Est cillum cillum laboris proident commodo irure qui ad. Voluptate magna proident et mollit eu laborum ea Lorem voluptate. Sunt excepteur eiusmod ad anim adipisicing. Est exercitation labore nulla irure ex nulla veniam velit aliquip voluptate proident.",
           "ownerName": "Collier Palmer",
@@ -86,12 +61,7 @@ angular.module('myApp', ['mainservice', 'ngRoute'])
         {
           "guid": "193d4231-9a43-471f-9217-282f2650545b",
           "name": "Phillips",
-          "address": {
-            "number": 211,
-            "road": "Gotham Avenue",
-            "city": "Germanton",
-            "state": "Texas"
-          },
+          "address": "dfhsdkfhsdjfbsjkskbfsd",
           "email": "a.a@gmail.com",
           "about": "Sunt duis anim id enim consequat officia proident. Excepteur sint fugiat in occaecat elit voluptate. Elit reprehenderit commodo dolor do cillum incididunt pariatur sit quis dolore. Cupidatat aute laboris ea aliqua laboris occaecat.",
           "ownerName": "Ramona Singleton",
@@ -105,21 +75,21 @@ angular.module('myApp', ['mainservice', 'ngRoute'])
     function ($routeProvider, $locationProvider) {
       $locationProvider.html5Mode(true);
       $routeProvider
-      .when('/index.html', {
-        templateUrl: 'views/restaurant/restau.html',
-        controller: 'MainController'
-      })
-      .when('/', {
-        templateUrl: 'views/restaurant/restau.html', //base (entry point html)
-        controller: 'MainController'
-      })
-      .when('/best', {
-        templateUrl: 'views/best/best.html',
-        controller: 'MainController'
-      })
-      .when('/edit/:id', {
-        templateUrl: 'views/edit/edit.html',
-        controller: 'MainController'
-      })
-      .otherwise({ redirectTo: '/' });
+        .when('/index.html', {
+          templateUrl: 'views/restaurant/restau.html',
+          controller: 'MainController'
+        })
+        .when('/', {
+          templateUrl: 'views/restaurant/restau.html', //base (entry point html)
+          controller: 'MainController'
+        })
+        .when('/best', {
+          templateUrl: 'views/best/best.html',
+          controller: 'MainController'
+        })
+        .when('/edit/:id', {
+          templateUrl: 'views/edit/edit.html',
+          controller: 'MainController'
+        })
+        .otherwise({ redirectTo: '/' });
     }]);
--- app/views/restaurant/addrestau/addrestau.js
+++ app/views/restaurant/addrestau/addrestau.js
@@ -8,10 +8,28 @@ angular.module('myApp').directive('addrestau', [function () {
         },
         link: function (scope) {
             //function when a restaurant is selected
-            scope.addrestau = function () {
-                scope.scope = true
-                console.log("adding")
+
+            scope.$watch(() => scope.restaudetail, function () {
+                scope.myrestau = angular.copy(scope.restaudetail);
+            }),
+
+                //Validate button
+                scope.valider = function () {
+                    console.log("adding");
+                    console.log(scope.tmprestau);
+                    scope.tmprestau.guid = uuidv4()
+                    scope.restaurants.push(scope.tmprestau);
+                    console.log(scope.restaurants);
+                }
+
+
+            function uuidv4() {
+                return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                    return v.toString(16);
+                });
             }
+
         }
     };
 }]);
